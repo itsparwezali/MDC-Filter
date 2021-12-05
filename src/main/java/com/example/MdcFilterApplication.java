205bdcb0399953a1ae1efc4aa39ce1efcc2d7888
itsparwezali <itsparwezali@gmail.com>
2021-12-05 12:55:55 +0545

    random generator token

--- src/main/java/com/example/MdcFilterApplication.java
+++ src/main/java/com/example/MdcFilterApplication.java
@@ -3,11 +3,18 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+import java.util.UUID;
+
 @SpringBootApplication
 public class MdcFilterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MdcFilterApplication.class, args);
+		String token=UUID.randomUUID().toString();
+		System.out.println(token);
+		System.out.println(token.toUpperCase().replace("-",""));
+		System.out.println(token.substring(5,8));
 	}
 
 }
